4b9c2f1dd48b1e745bd48597711b9bb32f37b948
prorooter007 <shreyashwasnik112@gmail.com>
2020-08-13 23:59:32 +0530

    prima:core: add missing bracket to fix compiler errors
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1681:3: error: expected expression
    
      else
    
      ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1761:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1840:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1909:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1945:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1982:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2033:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2184:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2244:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2339:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2386:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2846:1: error: function definition is not allowed here
    
    {
    
    ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:2897:2: error: expected '}'
    
    }
    
     ^
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c:1575:1: note: to match this '{'
    
    {
    
    ^
    
    13 errors generated.
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>
    
    prima:core:remove extra brackets to fix compiler warnings
    
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c:1710:15: error: expected expression
                } else if (psessionEntry->htCapability &&
                  ^
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c:1734:5: error: expected expression
        else
        ^
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c:1871:37: error: expected '}'
    } /*** end limSendMlmAssocInd() ***/
                                        ^
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c:1534:1: note: to match this '{'
    {
    ^
    3 errors generated.
    ../scripts/Makefile.build:339: recipe for target 'drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.o' failed
    make[4]: *** [drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.o] Error 1
    make[4]: *** Waiting for unfinished jobs....
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>
    
    prima:core: remove extra bracket
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
@@ -1862,7 +1862,7 @@ void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession p
                                                 eHT_CHANNEL_WIDTH_40MHZ) &&
                                   pAssocReq->HTCaps.supportedChannelWidthSet) {
                     pMlmAssocInd->chan_info.info = MODE_11AC_VHT40;
-                } else {
+                } else
                     pMlmAssocInd->chan_info.info = MODE_11AC_VHT20;
                     pMlmAssocInd->VHTCaps = pAssocReq->VHTCaps;
             } else if (psessionEntry->htCapability &&
@@ -1871,9 +1871,9 @@ void limSendMlmAssocInd(tpAniSirGlobal pMac, tpDphHashNode pStaDs, tpPESession p
                                                 eHT_CHANNEL_WIDTH_40MHZ) &&
                                   pAssocReq->HTCaps.supportedChannelWidthSet) {
                     pMlmAssocInd->chan_info.info = MODE_11NA_HT40;
-                } else {
+                } else
                     pMlmAssocInd->chan_info.info = MODE_11NA_HT20;
-                pMlmAssocInd->HTCaps = pAssocReq->HTCaps; }
+                pMlmAssocInd->HTCaps = pAssocReq->HTCaps;
             } else
                 pMlmAssocInd->chan_info.info = MODE_11A;
         }
